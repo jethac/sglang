a9a97342543002a279cb55cc819a1399f73ed2bf
Jetha Chan <jethachan@gmail.com>
2026-06-08 01:57:36 +0900

    Allow SM12x FlashInfer FP4 KV gates
    Signed-off-by: Jetha Chan <jethachan@gmail.com>
    (cherry picked from commit c5384d08b8fdcd4db507c44b5a23fdbeeca50659)

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -3282,6 +3282,8 @@ class ServerArgs:
                             "torch_native",
                             "flex_attention",
                         ]
+                        if is_sm120_supported():
+                            KV4_FA4_MHA_BACKEND_CHOICES.append("flashinfer")
                         assert (
                             self.decode_attention_backend_str
                             in KV4_FA4_MHA_BACKEND_CHOICES
@@ -3310,6 +3312,8 @@ class ServerArgs:
                             "flex_attention",
                             "trtllm_mha",
                         ]
+                        if is_sm120_supported():
+                            KV4_ATTENTION_MHA_BACKEND_CHOICES.append("flashinfer")
                         assert (
                             self.attention_backend in KV4_ATTENTION_MHA_BACKEND_CHOICES
                         ), (
