b40965eb09e8a09a3f2d2bb90d74672639e5b7d9
Jetha Chan <jethachan@gmail.com>
2026-06-11 22:06:32 +0900

    Set DiffusionGemma uniform dLLM flag

--- python/sglang/srt/dllm/config.py
+++ python/sglang/srt/dllm/config.py
@@ -48,12 +48,14 @@ class DllmConfig:
 
         arch = model_config.hf_config.architectures[0]
         if arch == "DiffusionGemmaForBlockDiffusion":
+            params = DLLM_PARAMS[arch]
             block_size = getattr(model_config.hf_config, "canvas_length", 256)
             mask_id = getattr(
                 model_config.hf_config,
                 "mask_token_id",
                 getattr(model_config.hf_config, "pad_token_id", 0),
             )
+            is_uniform = params["is_uniform"]
         elif arch in DLLM_PARAMS:
             params = DLLM_PARAMS[arch]
             block_size = params["block_size"]
